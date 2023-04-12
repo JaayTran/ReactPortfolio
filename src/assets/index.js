425d08f12ac7c04bbad0ef02efa38e6a1467d6c0
Jimmy Vu Tran <38023521+JaayTran@users.noreply.github.com>
2023-04-12 15:42:16 -0400

    init

---
+++ src/assets/index.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+
+import canfar from './company/canfar.png';
+import freelance from './company/freelance.png';
+
+import pos from './pos.png';
+import robotboil from './robotboil.png';
+import trianglefab from './trianglefab.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  canfar,
+  freelance,
+  pos,
+  robotboil,
+  trianglefab,
+};
